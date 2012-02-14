4d17e4bf073edbaf516435f1057892531acf2ed6
Tamara <tmasopust@student.tgm.ac.at>
2012-02-14 19:28:44 +0100

    implemented RegistrationStarter to only load XML file

--- CardTerminals.Tests/utils/RegistrationStarter.cs
+++ CardTerminals.Tests/utils/RegistrationStarter.cs
@@ -1,21 +1,16 @@
 using System;
 using System.Xml;
 using System.Linq;
+using System.IO;
 
 namespace Wiffzack.Devices.CardTerminals.Tests
 {
-	public class RegistrationStarter : Starter
+	/// <summary>
+	/// Registration starter. 
+	/// The registration starter awaits a XML file that contains the configuration for transport and registration.
+	/// </summary>
+	public class RegistrationStarter
 	{
-		bool configFlag = false;
-		XmlDocument config;
-		
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Wiffzack.Devices.CardTerminals.Tests.RegistrationStarter"/> class.
-		/// </summary>
-		public RegistrationStarter ()
-		{
-			config = new XmlDocument();
-		}
 		
 		/// <summary>
 		/// The entry point of the program, where the program control starts and ends.
@@ -24,83 +19,23 @@ namespace Wiffzack.Devices.CardTerminals.Tests
 		/// The command-line arguments.
 		/// </param>
 		static void Main(string[] args){
-			RegistrationStarter rs = new RegistrationStarter();
-			
-			if(rs.checkArgs(args)){
-				//arguments passed the test
-			}
-			if(!rs.configFlag){
-				rs.generateXML(args);
-			}
-		}
-		
-		/// <summary>
-		/// Checks the arguments.
-		/// </summary>
-		/// <returns>
-		/// The arguments.
-		/// </returns>
-		public bool checkArgs(string[] args){
-			if(args.Contains("--help")){
-				printHelp();
-				return true;
+			//check if the first argument is a file
+			if(args.Length!=1 || !File.Exists(args[0])){
+				Console.WriteLine("Please pass a XML configuration file as first argument!");
+				return;
 			}
 			
-			//check if config switch is set
-			if (args.Contains("-c")) {
-				configFlag = true;
-				//use given XML file
-    			int indexOfFile = Array.IndexOf(args,"-c")+1;
-				string xmlFile = args[indexOfFile];
-				
-				//read in xml file with xmltextreader and load to xmldocument
-				XmlTextReader reader = new XmlTextReader(xmlFile);
-				config.Load(reader);
-				return true;
-			}
-			return false;
-		}
-		
-		/// <summary>
-		/// Generates the XML document.
-		/// </summary>
-		/// <returns>
-		/// The XML document.
-		/// </returns>
-		public XmlDocument generateXML(string[] args){
-			return null;
-		}
-		
-		/// <summary>
-		/// Execute the command.
-		/// </summary>
-		public void execute(){
+			//load the XML file
+			XmlDocument config = new XmlDocument();
+			try {
+				config.Load(args[0]);
+ 			}
+ 			catch {	//if any exception occur, the XML file could not be read and thus the program stops
+				Console.WriteLine("Your XML was probably bad...");
+				return;
+ 			}
 			
-		}
-		
-		/// <summary>
-		/// Prints the help.
-		/// </summary>
-		public void printHelp(){
-			Console.WriteLine("Help for starting Registration:");
-			Console.WriteLine("-c <file>  ... use given configuration file");
-			Console.WriteLine("Transport settings:");
-			Console.WriteLine("-t <type>  ... use the defined transport type (Serial or Network)");
-			Console.WriteLine("-sp <port> ... define serial port");
-			Console.WriteLine("-br <int>  ... define BaudRate");
-			Console.WriteLine("-pr <val>  ... define Parity (None, Odd, Even)");
-			Console.WriteLine("-sb <val>  ... define StopBits (One, Two)");
-			Console.WriteLine("-db <int>  ... define DataBits (5,6,7,8)");
-			Console.WriteLine("-rb <int>  ... define ReadBuffer");
-			Console.WriteLine("-wb <int>  ... define WriterBuffer");
-			Console.WriteLine("-ip <val>  ... define remote IP (only in Network transport)");
-			Console.WriteLine("-np <port> ... define network port");
-			Console.WriteLine("Registration commands:");
-			Console.WriteLine("-pa <val>  ... activate/deactivate printing of administration receipts on ECR (True, False)");
-			Console.WriteLine("-pp <val>  ... activate/deactivate printing of payment receipts on ECR (True, False)");
-			Console.WriteLine("-da <val>  ... disable amount input on PT (True, False)");
-			Console.WriteLine("-df <val>  ... disable administration functions on PT (True, False)");
+			Console.WriteLine("XML file loaded");			
 		}
 	}
 }
-
